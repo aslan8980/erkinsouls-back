6301ac5cdddff28ce07a180e20b01d9d8549cf63
aslan8980 <aslan.daiyrov@gmail.com>
2026-02-26 16:23:02 +0100

    Register orders router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.products import router as products_router
+from app.api.cart import router as cart_router
+from app.api.orders import router as orders_router
 
 app = FastAPI(title="MenStore API", version="0.1.0")
 
@@ -13,7 +15,8 @@ app.add_middleware(
 )
 
 app.include_router(products_router)
-
+app.include_router(orders_router)
+app.include_router(cart_router)
 
 @app.get("/")
 def root():
